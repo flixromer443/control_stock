d51ffd0bbfb24ba0eb5304d97c83d27a47f73b9d
Felix Etchegaray <flixromer443@gmail.com>
2022-09-23 22:00:24 -0300

    agrego archivo para conexion a base de datos mas legible

---
+++ control_stock/config/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace control_stock.config
+{
+    internal class Conexion
+    {
+        private static string datasource = "Data Source=control_stockV01.db";
+        public SQLiteConnection conectar()
+        {
+            return new SQLiteConnection(datasource);
+        }
+    }
+    
+}
