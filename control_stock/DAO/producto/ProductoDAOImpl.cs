0db52645103fc4fd8762975729e7e2b57a646bba
Felix Etchegaray <flixromer443@gmail.com>
2022-10-02 14:26:28 -0300

    agrego correccion para que el stock no quede en -1 nunca

--- control_stock/DAO/producto/ProductoDAOImpl.cs
+++ control_stock/DAO/producto/ProductoDAOImpl.cs
@@ -78,7 +78,11 @@ namespace control_stock.services.producto_service
                 command.Parameters.Add(new SQLiteParameter("@DESCRIPCION", productoDTO.Descripcion));
                 command.Parameters.Add(new SQLiteParameter("@PRECIO_COMPRA", productoDTO.PrecioCompra));
                 command.Parameters.Add(new SQLiteParameter("@PRECIO_VENTA", productoDTO.PrecioVenta));
-                command.Parameters.Add(new SQLiteParameter("@STOCK", productoDTO.Stock));
+
+                //no es lo ideal, ya lo se
+                int stock = (productoDTO.Stock < 1) ? 0 : productoDTO.Stock;
+                command.Parameters.Add(new SQLiteParameter("@STOCK", stock));
+
                 command.ExecuteNonQuery();
             }
             connection.Close();
