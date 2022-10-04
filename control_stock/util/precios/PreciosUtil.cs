f79da7b5fe400d10c87ad14d01e6a1fde6ccc37c
Felix Etchegaray <flixromer443@gmail.com>
2022-10-04 15:46:37 -0300

    creo PreciosUtil

---
+++ control_stock/util/precios/PreciosUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace control_stock.util.precios
+{
+    internal class PreciosUtil
+    {
+        public static string generarPrecio(string precio)
+        {
+            StringBuilder precioVenta = new StringBuilder();
+            precioVenta.Append("$" + precio);
+            return precioVenta.ToString();
+        }
+    }
+}
